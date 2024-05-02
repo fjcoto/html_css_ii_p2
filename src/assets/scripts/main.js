3a3d6eb9f552e375a0b7025923425561f9364018
Francisco José Coto Pacheco <fcoto@uoc.edu>
2024-05-02 22:55:13 +0200

    P2 modified

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -23,43 +23,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Index selectors
   const overlay = document.querySelector('.overlay');
+  const toggler = document.querySelector('.navbar-toggler');
 
-  // Header selectors
-  const menuButton = document.querySelector('.header__menu-button');
-  const navContainer = document.querySelector('.header__nav');
-  const iconBar = document.querySelector('.fa-bars');
-  const iconTimes = document.querySelector('.fa-times');
+  toggler.addEventListener('click', function(){
 
-  function toggleMenu() {
-    if (navContainer.style.transform === 'translateX(-100%)' || !navContainer.style.transform) {
-      navContainer.style.transform = 'translateX(0%)';
-      iconBar.style.display = 'none';
-      iconTimes.style.display = 'block';
+    const isExpanding = !document.getElementById('navbarNav').classList.contains('show');
+
+    if(isExpanding){
       overlay.style.display = 'block';
-    } else {
-      navContainer.style.transform = 'translateX(-100%)';
-      iconBar.style.display = 'block';
-      iconTimes.style.display = 'none';
+    }else{
       overlay.style.display = 'none';
     }
-
-  }
-
-  menuButton.addEventListener('click', toggleMenu);
-
-  // Active menu option
-  const path = window.location.pathname;
-  const options = document.querySelectorAll('.header__nav-item > a');
-
-  options.forEach(function (option) {
-    if (option.getAttribute('href') === path) {
-      option.classList.add('active');
-    }
-
-    if (path === '/detail.html') {
-      options[1].classList.add('active');
-    }
-
+      
   });
 
 });

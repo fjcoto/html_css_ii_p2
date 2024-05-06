687bf27533ee422e5e56368a762bcf31fa1077a8
Francisco José Coto Pacheco <fcoto@uoc.edu>
2024-05-06 22:24:09 +0200

    P2 created

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -62,4 +62,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
   });
 
+  // Init Tooltip
+  const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+  const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 });
